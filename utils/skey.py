58e2ac80cd7f3324dc5c28bd067878ad800735eb
Fabian Martinez Portantier <fabian@portantier.com>
2018-07-18 16:10:48 -0300

    python 3.5 compatibility (random instead secrets)

--- utils/skey.py
+++ utils/skey.py
@@ -1,10 +1,12 @@
 import hashlib
-import secrets
+import random
+#import secrets # needs python 3.6
 
 OTPNUM = 50
 ALGORITHM = 'sha1'
 
-key = secrets.token_bytes(2048)
+#key = secrets.token_bytes(2048) # needs python 3.6
+key = str(random.getrandbits(2048)).encode()
 
 lastkey = hashlib.new(ALGORITHM, key).hexdigest()
 
