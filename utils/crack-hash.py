dc7435b4b76ee753594dc64a89136b5fc5e75551
Fabian Martinez Portantier <fabian@portantier.com>
2019-04-09 19:13:15 -0300

    migrated to click

--- utils/crack-hash.py
+++ utils/crack-hash.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 import hashlib
-import sys
 
-candidate = sys.argv[1]
+import click
 
-for number in range(10000):
-    h = hashlib.sha512(str(number).encode()).hexdigest()
-    if h == candidate:
-        print('Cracked! Password:', number)
 
+@click.command()
+@click.argument('algorithm', type=click.Choice(sorted(hashlib.algorithms_available)))
+@click.argument('digest')
+def crack_hash(digest, algorithm):
+
+    for number in range(10000):
+        h = hashlib.new(algorithm, str(number).encode()).hexdigest()
+        if h == digest:
+            print('Cracked! Password:', number)
+            return True
+
+    print('Unable to crack', digest)
+    return False
+
+if __name__ == '__main__':
+    crack_hash()
