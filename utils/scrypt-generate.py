701cca8a03186de73d729befae32783fa460ac85
Fabian Martinez Portantier <fabian@portantier.com>
2019-04-09 19:23:26 -0300

    migrated to click

--- utils/scrypt-generate.py
+++ utils/scrypt-generate.py
@@ -4,39 +4,32 @@ import sys
 import os
 from binascii import hexlify
 
+import click
+
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 from cryptography.hazmat.backends import default_backend
 
-password = sys.argv[1].encode()
-backend = default_backend()
-salt = os.urandom(16)
+@click.command()
+@click.argument('message')
+def scrypt_generate(message):
 
-# derive
-kdf = Scrypt(
-    salt=salt,
-    length=32,
-    n=2**14,
-    r=8,
-    p=1,
-    backend=backend
-)
+    backend = default_backend()
+    salt = os.urandom(16)
 
-key = kdf.derive(password)
+    kdf = Scrypt(
+        salt=salt,
+        length=32,
+        n=2**14,
+        r=8,
+        p=1,
+        backend=backend
+    )
 
-print(hexlify(salt).decode(), hexlify(key).decode())
+    key = kdf.derive(message.encode())
+
+    print(hexlify(salt).decode(), hexlify(key).decode())
 
 
-'''
-# verify
-kdf = Scrypt(
-salt=salt,
-length=32,
-n=2**14,
-r=8,
-p=1,
-backend=backend
-)
-kdf.verify(b"my great password", key)
-'''
-
+if __name__ == '__main__':
+    scrypt_generate()
 
