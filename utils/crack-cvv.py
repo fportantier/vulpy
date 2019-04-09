9e33700b1f4c3efc7f2c260c837ed17c56129f3a
Fabian Martinez Portantier <fabian@portantier.com>
2019-04-09 18:53:17 -0300

    migrated to click

--- utils/crack-cvv.py
+++ utils/crack-cvv.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python3
+
 import sys
 import hashlib
 
-algorithm = sys.argv[1]
-h = sys.argv[2]
+import click
 
-for number in range(0, 1000):
-    cvv = "{:03}".format(number).encode()
-    result = hashlib.new(algorithm, cvv).hexdigest()
-    if h == result:
-        print('Cracked! CVV:', cvv.decode())
-        break
+@click.command()
+@click.argument('algorithm', type=click.Choice(sorted(hashlib.algorithms_available)))
+@click.argument('digest')
+def crack_cvv(algorithm, digest):
 
+    for number in range(0, 1000):
+        cvv = "{:03}".format(number).encode()
+        result = hashlib.new(algorithm, cvv).hexdigest()
+        if digest == result:
+            print('Cracked! CVV:', cvv.decode())
+            break
+
+
+if __name__ == '__main__':
+    crack_cvv()
