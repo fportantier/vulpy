567663f0fd2d23e04d239fee83dc8784fa86af41
Fabian Martinez Portantier <fabian@portantier.com>
2019-06-13 13:03:52 -0300

    email -> username

--- bad/vulpy.py
+++ bad/vulpy.py
@@ -4,14 +4,13 @@ from pathlib import Path
 
 from flask import Flask, g, redirect, request
 
-from mod_hello import mod_hello
-from mod_user import mod_user
-from mod_posts import mod_posts
-from mod_mfa import mod_mfa
-from mod_csp import mod_csp
-from mod_api import mod_api
-
 import libsession
+from mod_api import mod_api
+from mod_csp import mod_csp
+from mod_hello import mod_hello
+from mod_mfa import mod_mfa
+from mod_posts import mod_posts
+from mod_user import mod_user
 
 app = Flask('vulpy')
 app.config['SECRET_KEY'] = 'aaaaaaa'
@@ -34,7 +33,9 @@ if csp_file.is_file():
             line = line.replace('\n', '')
             if line:
                 csp += line
-        print('CSP:', csp)
+if csp:
+    print('CSP:', csp)
+
 
 @app.route('/')
 def do_home():
@@ -50,5 +51,5 @@ def add_csp_headers(response):
         response.headers['Content-Security-Policy'] = csp
     return response
 
-app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
 
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
