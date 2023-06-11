aad4a780a508c92123aea3e1a71c583cff2c539d
unkn0wn93develop <135967279+unkn0wn93develop@users.noreply.github.com>
2023-06-11 11:35:39 +0000

    Test

--- bad/vulpy.py
+++ bad/vulpy.py
@@ -52,4 +52,4 @@ def add_csp_headers(response):
     return response
 
 
-app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+app.run(debug=True, host='127.0.1.1', port=4999, extra_files='csp.txt')
